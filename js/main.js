bcf530654f29b9ba18bce48014a82a4bbd2c4275
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-08-03 11:51:09 -0400

    Feat: some styles for swiper

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// swiper 
+
+var swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  slidesPerView: 4,
+  slidesPerColumn: 2,
+  slidesPerGroup: 4,
+  slidesPerColumnFill: "row",
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+// swiper (end)
