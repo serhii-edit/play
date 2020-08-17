6d7578117ef6d817dbb6fbef48a278c9fd84f6a7
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-08-17 18:50:19 -0400

    Edit: change the script space work & modal open after send & close on hit ok

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ btn.on("click", function(e) {
   e.preventDefault();
   $("html, body").animate({scrollTop: 0}, "500");
 });
-// tow arrow end
+// top arrow end
 
 
 
@@ -85,4 +85,90 @@ var blogSwiper = new Swiper('.swiper-container-blog', {
   
 });
 // swiper (end)
+
+var modal = document.querySelector(".modal-thank");
+var modalBtn = document.querySelector(".modal-thank__button");
+
+
+
+$(".contactForm").validate({
+  errorClass: "error-input",
+  
+  rules: {
+   
+    userName: {
+      required: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+    userPhone: {
+      required: true,
+      // minlength: 2,
+      // maxlength: 100,
+    },
+    userEmail: {
+      required: true,
+      email: true,
+      maxlength: 100,
+    },
+
+    userMassage: {
+      required: true,
+      maxlength: 300,
+    },
+    
+  },
+
+  messages: {
+    userName: {
+      required: "Name is required",
+      minlength: "Full Name",
+    },
+    userPhone: {
+      required: "Phone is required",
+    },
+    userEmail: {
+      required: "Email is required",
+      email: "Format-Example: name@domain.com",
+    },
+    userMassage: {
+      required: "Type your massage",
+      maxlength: "Too much!",
+    },
+  },
+
+  submitHandler: function(form) {
+  $.ajax({
+    type: "POST",
+    url: "send.php",
+    data: $(form).serialize(),
+    // dataType: "dataType",
+    success: function (response) {
+      console.log("#1 Ajax worked, answer server:" + response);
+      alert("#1 Contact form has been Submited Succesful");
+      modal.style.visibility="visible";
+      $(form)[0].reset();
+    },
+    error: function (response) {
+      console.log("#1 The form does't work" + response);
+    }
+  });
+  }
+
+ });
+
+
+ // mask for phone input (start)
+
+ $('[type=tel]').mask('+1 (000) 000-0000', {placeholder: "+1 (___) ___-____"});
+
+ // mask for phone input (end)
+
+
+
+ modalBtn.onclick = function (event) {
+   if (event.target == modalBtn)
+   modal.style.visibility="hidden";
+ };
+
 
